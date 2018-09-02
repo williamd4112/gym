bed09eaac4a5220beb5912ce2feb5fb094fbb871
Hong Zhang-Wei <williamd@Hongde-MBP.fritz.box>
2018-09-02 15:48:38 +0200

    modify ant

--- gym/envs/mujoco/ant.py
+++ gym/envs/mujoco/ant.py
@@ -30,9 +30,10 @@ class AntEnv(mujoco_env.MujocoEnv, utils.EzPickle):
 
     def _get_obs(self):
         return np.concatenate([
-            self.sim.data.qpos.flat[2:],
+            self.sim.data.qpos.flat,
             self.sim.data.qvel.flat,
             np.clip(self.sim.data.cfrc_ext, -1, 1).flat,
+            self.get_body_com("torso")[0]
         ])
 
     def reset_model(self):
