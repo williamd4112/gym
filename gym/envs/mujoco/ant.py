af6ad7ba1620f809988193873bce2df4e6d322f2
williamd4112 <williamd4112@hotmail.com>
2018-12-22 21:24:55 +0800

    update ant reward func

--- gym/envs/mujoco/ant.py
+++ gym/envs/mujoco/ant.py
@@ -18,12 +18,14 @@ class AntEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         contact_cost = 0.5 * 1e-3 * np.sum(
             np.square(np.clip(self.sim.data.cfrc_ext, -1, 1)))
         survive_reward = 1.0  
+        
         #reward = forward_reward - ctrl_cost - contact_cost + survive_reward
-        reward = forward_reward
+        #reward = forward_reward
+        reward = forward_reward - ctrl_cost * 0.01 + survive_reward * 0.001
 
         state = self.state_vector()
         notdone = np.isfinite(state).all() \
-            and state[2] >= 0.2 and state[2] <= 1.0 # NOTE: old 0.2
+            and state[2] >= 0.3 and state[2] <= 1.0 # NOTE: old 0.2
         
         #done = not notdone
         done = False
